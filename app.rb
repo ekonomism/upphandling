579d20573de3455b5d5bb83297f17734404dd275
mattias.lundback <mattias.lundback@ekonomism.us>
2019-10-19 17:20:33 -0400

    Snabbare

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ $kodnyckel = {'A' => 1..3, 'B' => 5..9, 'C' => 10..33, 'D' => 35..35, 'E' => 36.
 
 DB = Sequel.connect('sqlite://foretag.db')
 def initiera_databas
-  # Skapar bara tables om de inte redan existerar
-  DB.create_table? :relationer do
+  # Skapar table och skriver över om den existerar
+  DB.create_table! :relationer do
     String :Lev, null: false
     String :Lev_namn
     String :Kop
@@ -38,6 +38,13 @@ def initiera_databas
   end
 end
 
+# Helper
+class String
+  def is_integer?
+    self.to_i.to_s == self
+  end
+end
+
 class Inkopare
   attr_reader :inkopsandel, :snittstorlek, :lokalandel, :privatandel
   
@@ -47,41 +54,77 @@ class Inkopare
   
   private
   
-  def inkopsandel # Andel inköp av kommunens totala omsättning
+  def inkopsandel(sni) # Andel inköp av kommunens totala omsättning
     
   end  
   
-  def snittstorlek # Snittstorlek på leverantörer vägt efter kontraktsstorlek
+  def snittstorlek(sni) # Snittstorlek på leverantörer vägt efter kontraktsstorlek
     
   end
   
-  def lokalandel # Andel som kommuner köper av lokala leverantörer
+  def lokalandel(sni) # Andel som kommuner köper av lokala leverantörer
     
   end  
   
-  def privatandel # Andel privat försäljning bland leverantörer
+  def privatandel(sni) # Andel privat försäljning bland leverantörer
     
   end
   
 end  
 
-poster = DB[:relationer]
-rad = 0
-CSV.foreach("foretag_ar_2017.csv", :encoding => 'iso-8859-1', headers: true) do |foretag|
-  rad += 1
-  puts rad, foretag[8].encode(Encoding::UTF_8)
-  (1..100).each do |sni|
+def rensa(lista)
+  lista.each_with_index do |post, index|
+    post = "" if post.nil?
+    lista[index] = post.encode(Encoding::UTF_8).strip
+  end
+  return lista
+end
+
+def skapa_databas
+  initiera_databas
+  poster = DB[:relationer]
+  (1..1).each do |sni|
+    puts sni
     filnamn = "SNI" + sni.to_s + ".csv"
     if File.file?(filnamn) then
-      CSV.foreach(filnamn, :encoding => 'iso-8859-1', :col_sep => ";", headers: true) do |relation|
-        if foretag[0] == relation[0] then
-          puts "Relation:", foretag, relation
-          relationer.insert(Lev: relation[0].encode(Encoding::UTF_8), Lev_namn: relation[1].encode(Encoding::UTF_8), Kop: relation[2].encode(Encoding::UTF_8), Kop_namn: relation[3].encode(Encoding::UTF_8), Typ: relation[4].encode(Encoding::UTF_8), Summa: relation[9].encode(Encoding::UTF_8), AFakt: relation[10].encode(Encoding::UTF_8), SNI: relation[11].encode(Encoding::UTF_8), SNI_namn: relation[13].encode(Encoding::UTF_8), Lan: foretag[6].encode(Encoding::UTF_8), Kommun: foretag[7].encode(Encoding::UTF_8), Stlk_klass: foretag[10].encode(Encoding::UTF_8), Reg_datum: foretag[16].encode(Encoding::UTF_8), Anstallda: foretag[35].encode(Encoding::UTF_8), Omsattning: foretag[43].encode(Encoding::UTF_8), RRes: foretag[55].encode(Encoding::UTF_8), ARes: foretag[76].encode(Encoding::UTF_8)) 
+      CSV.foreach(filnamn, :encoding => 'iso-8859-1', :col_sep => ";") do |relation|
+        relation = rensa(relation)
+        if relation[9].is_integer? then
+          poster.insert(Lev: relation[0], Lev_namn: relation[1], Kop: relation[2], Kop_namn: relation[3], Typ: relation[4], Summa: relation[9], AFakt: relation[10], SNI: relation[11], SNI_namn: relation[12])
         end
       end
     end
   end
+  puts "Klar relationer"
+  nummer = 0
+  CSV.foreach("foretag_ar_2017.csv", :encoding => 'iso-8859-1') do |foretag|
+    foretag = rensa(foretag)
+    nummer += 1
+    puts nummer if nummer % 10000 == 0
+    poster.where(Lev: foretag[0]).update(Lan: foretag[6], Kommun: foretag[7], Stlk_klass: foretag[10], Reg_datum: foretag[16], Anstallda: foretag[35], Omsattning: foretag[43], RRes: foretag[55], ARes: foretag[76])
+  end    
+  puts "Klar företag"
+  $kodnyckel.each do |key, value|
+    nedre = value.first * 1000 - 1
+    ovre = (value.last + 1) * 1000
+    puts $kodnyckel[key]
+    poster.where(SNI: nedre..ovre).update(SNI_A: key)
+  end  
+  puts "Klar avdelning"
+  CSV.foreach("kommuner.csv") do |kommun|
+    kommun = rensa(kommun)
+    puts kommun[0][0..3].sub!(/^0+/, "")
+    poster.where(Kommun: kommun[0][0..3].sub!(/^0+/, ""), Typ: "Kommun").update(HOms: -kommun[2].to_i)
+  end  
+  CSV.foreach("landsting.csv") do |landsting|
+    landsting = rensa(landsting)
+    puts landsting[0], landsting[0][0..1].sub!(/^0+/, "")
+    poster.where(Lan: landsting[0][0..1].sub!(/^0+/, ""), Typ: "Landsting").update(HOms: -landsting[2].to_i)
+  end  
+  puts "Klar kommuner"
 end
 
 
+
+skapa_databas
 
